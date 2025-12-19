973ebedb91123f383e91083ff1017568178de8c7
Dan1scum <belizer808@gmail.com>
2025-12-19 12:49:39 +0600

    upddate

---
+++ lesson/lesson6.py
@@ -0,0 +1,11 @@
+# Встроенный модули PYTHON, определенние собствееных модулей, внешние модули и их устоновка.
+
+# print(randint(1, 10))
+
+# import lesson1
+
+# hero = lesson1.Phone('Apple', 'iPhone 13', 100)
+#
+# print(hero.name)
+
+
